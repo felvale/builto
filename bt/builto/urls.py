c0aa3dd87a2e4529a43b91f5371d21c4dbccf909
hj539 <felvale@hotmail.com>
2017-07-15 00:41:10 -0300

    starting to configure urls and index view

--- bt/builto/urls.py
+++ bt/builto/urls.py
@@ -2,8 +2,9 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^', include('builto.urls'))
+    url(r'^$', views.home_page, name='home')
 ]
